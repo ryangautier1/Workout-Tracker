0b16763cef63bf82e2fabbcb9726692c1a45b7b8
Ryan Gautier <ryangautier2@gmail.com>
2020-05-06 16:50:40 -0500

    fixing routes

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -5,14 +5,45 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    default: Date.now
+    default: Date.now()
   },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"       
-    }
-  ]
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: "Exercise type is required"
+    },
+
+    name : {
+      type: String,
+      trim: true
+    },
+
+    duration: {
+      type: Number
+    },
+
+    weight: {
+      type: Number
+    },
+    
+    distance: {
+      type: Number
+    },
+
+    reps: {
+      type: Number
+    },
+
+    sets: {
+      type: Number
+    },
+
+    // {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "Exercise"       
+    // }
+  }]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+var path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,28 +20,73 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 // routes
 
-//route for all workouts
-app.get("/api/workouts", (req,res) => {
-  db.workouts.find({}, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
+//route for getting all workouts
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(data => {
       res.json(data);
-    }
-  })
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-// route for workout by id
-app.get("/api/workouts/:id", (req,res) => {
-  db.workouts.find({_id: mongojs.ObjectId(req.params.id)}, (err, data) => {
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// route for getting workout by id
+app.get("/api/workouts/:id", (req, res) => {
+  db.Workout.findById(req.params.id)
+    .then(data => {
+      console.log(res.json(data));
+      res.json(data);
+    })
+    .catch(err => {
+      console.log("oops");
+      res.json(err);
+    });
+});
+
+
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: [req.body] } },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.send(data);
+      }
+    });
+});
+
+// route for posting new workout
+app.post("/api/workouts", (req, res) => {
+  db.Workout.create({}, (err, data) => {
     if (err) {
       console.log(err);
     }
     else {
-      res.json(data);
+      res.send(data);
     }
-  })
+  });
+});
+
+
+// html routes
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
 });
 
 app.listen(PORT, () => {
